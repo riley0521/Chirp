9de8a9ed131861ea936b49ee172b1c599b289f32
Riley Farro <rileyfarro@gmail.com>
2025-10-16 23:13:33 +0800

    Create build-logic convention plugin.

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,27 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    `kotlin-dsl`
+}
+
+group = "com.rfcoding.convention.buildlogic"
+
+dependencies {
+    compileOnly(libs.android.gradle.plugin)
+    compileOnly(libs.android.tools.common)
+    compileOnly(libs.compose.gradle.plugin)
+    compileOnly(libs.kotlin.gradle.plugin)
+    compileOnly(libs.androidx.room.gradle.plugin)
+    compileOnly(libs.ksp.gradle.plugin)
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_17
+    }
+}
