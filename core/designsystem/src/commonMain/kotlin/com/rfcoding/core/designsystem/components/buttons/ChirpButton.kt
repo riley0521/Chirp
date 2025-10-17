34d4cf2346eafac05a3dce669804b43c153df828
Riley Farro <rileyfarro@gmail.com>
2025-10-17 23:52:11 +0800

    Setup core buttons.

---
+++ core/designsystem/src/commonMain/kotlin/com/rfcoding/core/designsystem/components/buttons/ChirpButton.kt
@@ -0,0 +1,195 @@
+package com.rfcoding.core.designsystem.components.buttons
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.rfcoding.core.designsystem.theme.ChirpTheme
+import com.rfcoding.core.designsystem.theme.extended
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+enum class ChirpButtonStyle {
+    PRIMARY,
+    DESTRUCTIVE_PRIMARY,
+    SECONDARY,
+    DESTRUCTIVE_SECONDARY,
+    TEXT
+}
+
+@Composable
+fun ChirpButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    style: ChirpButtonStyle = ChirpButtonStyle.PRIMARY,
+    enabled: Boolean = true,
+    isLoading: Boolean = false,
+    leadingIcon: @Composable (() -> Unit)? = null
+) {
+    val colors = when (style) {
+        ChirpButtonStyle.PRIMARY -> ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            contentColor = MaterialTheme.colorScheme.onPrimary,
+            disabledContainerColor = MaterialTheme.colorScheme.extended.disabledFill,
+            disabledContentColor = MaterialTheme.colorScheme.extended.textDisabled
+        )
+        ChirpButtonStyle.DESTRUCTIVE_PRIMARY -> ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.error,
+            contentColor = MaterialTheme.colorScheme.onError,
+            disabledContainerColor = MaterialTheme.colorScheme.extended.disabledFill,
+            disabledContentColor = MaterialTheme.colorScheme.extended.textDisabled
+        )
+        ChirpButtonStyle.SECONDARY -> ButtonDefaults.buttonColors(
+            containerColor = Color.Transparent,
+            contentColor = MaterialTheme.colorScheme.extended.textSecondary,
+            disabledContainerColor = Color.Transparent,
+            disabledContentColor = MaterialTheme.colorScheme.extended.textDisabled
+        )
+        ChirpButtonStyle.DESTRUCTIVE_SECONDARY -> ButtonDefaults.buttonColors(
+            containerColor = Color.Transparent,
+            contentColor = MaterialTheme.colorScheme.error,
+            disabledContainerColor = Color.Transparent,
+            disabledContentColor = MaterialTheme.colorScheme.extended.textDisabled
+        )
+        ChirpButtonStyle.TEXT -> ButtonDefaults.buttonColors(
+            containerColor = Color.Transparent,
+            contentColor = MaterialTheme.colorScheme.tertiary,
+            disabledContainerColor = Color.Transparent,
+            disabledContentColor = MaterialTheme.colorScheme.extended.textDisabled
+        )
+    }
+
+    val defaultBorderStroke = BorderStroke(
+        width = 1.dp,
+        color = MaterialTheme.colorScheme.extended.disabledOutline
+    )
+    val border = when {
+        style == ChirpButtonStyle.PRIMARY && !enabled -> defaultBorderStroke
+        style == ChirpButtonStyle.SECONDARY -> defaultBorderStroke
+        style == ChirpButtonStyle.DESTRUCTIVE_PRIMARY && !enabled -> defaultBorderStroke
+        style == ChirpButtonStyle.DESTRUCTIVE_SECONDARY -> {
+            BorderStroke(
+                width = 1.dp,
+                color = if (enabled) {
+                    MaterialTheme.colorScheme.extended.destructiveSecondaryOutline
+                } else {
+                    MaterialTheme.colorScheme.extended.disabledOutline
+                }
+            )
+        }
+        else -> null
+    }
+
+    Button(
+        onClick = onClick,
+        modifier = modifier,
+        enabled = enabled,
+        shape = RoundedCornerShape(8.dp),
+        colors = colors,
+        border = border
+    ) {
+        Box(
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator(
+                modifier = Modifier
+                    .size(15.dp)
+                    .alpha(
+                        alpha = if (isLoading) 1f else 0f
+                    ),
+                strokeWidth = 1.5.dp
+            )
+        }
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(
+                8.dp,
+                Alignment.CenterHorizontally
+            ),
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .alpha(
+                    alpha = if (isLoading) 0f else 1f
+                )
+        ) {
+            leadingIcon?.invoke()
+            Text(
+                text = text,
+                style = MaterialTheme.typography.titleSmall
+            )
+        }
+    }
+}
+
+@Composable
+@Preview
+private fun ChirpPrimaryButtonPreview() {
+    ChirpTheme {
+        ChirpButton(
+            text = "Hello world!",
+            onClick = {}
+        )
+    }
+}
+
+@Composable
+@Preview
+private fun ChirpDestructivePrimaryButtonPreview() {
+    ChirpTheme {
+        ChirpButton(
+            text = "Hello world!",
+            onClick = {},
+            style = ChirpButtonStyle.DESTRUCTIVE_PRIMARY
+        )
+    }
+}
+
+@Composable
+@Preview
+private fun ChirpSecondaryButtonPreview() {
+    ChirpTheme {
+        ChirpButton(
+            text = "Hello world!",
+            onClick = {},
+            style = ChirpButtonStyle.SECONDARY
+        )
+    }
+}
+
+@Composable
+@Preview
+private fun ChirpDestructiveSecondaryButtonPreview() {
+    ChirpTheme {
+        ChirpButton(
+            text = "Hello world!",
+            onClick = {},
+            style = ChirpButtonStyle.DESTRUCTIVE_SECONDARY
+        )
+    }
+}
+
+@Composable
+@Preview
+private fun ChirpTextButtonPreview() {
+    ChirpTheme {
+        ChirpButton(
+            text = "Hello world!",
+            onClick = {},
+            style = ChirpButtonStyle.TEXT
+        )
+    }
+}
