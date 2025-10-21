7b8e2a1211d48c7b35de7247cecd96eee64168af
Riley Farro <rileyfarro@gmail.com>
2025-10-21 23:48:10 +0800

    Setup splash screen for Android.

--- composeApp/src/androidMain/kotlin/com/rfcoding/chirp/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/rfcoding/chirp/MainActivity.kt
@@ -4,11 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
@@ -16,10 +16,4 @@ class MainActivity : ComponentActivity() {
             App()
         }
     }
-}
-
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
 }
