242692f3485d2cb604671901bc623abf0e410993
Riley Farro <rileyfarro@gmail.com>
2025-10-29 00:28:28 +0800

    Add ChirpAdaptiveDialogSheetLayout.

---
+++ core/designsystem/src/commonMain/kotlin/com/rfcoding/core/designsystem/components/dialogs/ChirpAdaptiveDialogSheetLayout.kt
@@ -0,0 +1,24 @@
+package com.rfcoding.core.designsystem.components.dialogs
+
+import androidx.compose.runtime.Composable
+import com.rfcoding.core.presentation.util.currentDeviceConfiguration
+
+@Composable
+fun ChirpAdaptiveDialogSheetLayout(
+    onDismiss: () -> Unit,
+    content: @Composable () -> Unit
+) {
+    val configuration = currentDeviceConfiguration()
+
+    if (configuration.isMobile) {
+        ChirpBottomSheet(
+            onDismiss = onDismiss,
+            content = content
+        )
+    } else {
+        ChirpDialogContent(
+            onDismiss = onDismiss,
+            content = content
+        )
+    }
+}
