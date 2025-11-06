741a708e1f265d6281944b895413166725b76aff
Riley Farro <rileyfarro@gmail.com>
2025-11-07 05:46:00 +0800

    Setup ChatParticipantDao.

---
+++ feature/chat/database/src/commonMain/kotlin/com/rfcoding/chat/database/dao/ChatParticipantDao.kt
@@ -0,0 +1,19 @@
+package com.rfcoding.chat.database.dao
+
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Upsert
+import com.rfcoding.chat.database.entities.ChatParticipantEntity
+
+@Dao
+interface ChatParticipantDao {
+
+    @Upsert
+    suspend fun upsertParticipant(participant: ChatParticipantEntity)
+
+    @Upsert
+    suspend fun upsertParticipants(participants: List<ChatParticipantEntity>)
+
+    @Query("SELECT * FROM chat_participants")
+    suspend fun getAllParticipants(): List<ChatParticipantEntity>
+}
