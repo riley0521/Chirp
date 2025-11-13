ba27a0a3c45c8547447f5123936751d0c97e6ed7
Riley Farro <rileyfarro@gmail.com>
2025-11-14 00:40:35 +0800

    - Remove hard coded padding in MessageBox - Fix shadow and add DynamicRoundedCornerColumn in MessageBox for bigger devices.

--- feature/chat/presentation/src/commonMain/kotlin/com/rfcoding/chat/presentation/chat_detail/ChatDetailScreen.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/rfcoding/chat/presentation/chat_detail/ChatDetailScreen.kt
@@ -25,6 +25,7 @@ import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.backhandler.BackHandler
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -178,6 +179,10 @@ fun ChatDetailScreen(
                             },
                             modifier = Modifier
                                 .fillMaxWidth()
+                                .padding(
+                                    vertical = 8.dp,
+                                    horizontal = 16.dp
+                                )
                         )
                     }
                 }
@@ -189,16 +194,21 @@ fun ChatDetailScreen(
                 AnimatedVisibility(
                     visible = configuration.isWideScreen && state.chatUi != null
                 ) {
-                    MessageBox(
-                        messageTextFieldState = state.messageTextFieldState,
-                        isTextInputEnabled = state.canSendMessage,
-                        connectionState = state.connectionState,
-                        onSendClick = {
-                            onAction(ChatDetailAction.OnSendMessageClick)
-                        },
-                        modifier = Modifier
-                            .fillMaxWidth()
-                    )
+                    DynamicRoundedCornerColumn(
+                        isCornersRounded = true
+                    ) {
+                        MessageBox(
+                            messageTextFieldState = state.messageTextFieldState,
+                            isTextInputEnabled = state.canSendMessage,
+                            connectionState = state.connectionState,
+                            onSendClick = {
+                                onAction(ChatDetailAction.OnSendMessageClick)
+                            },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(8.dp)
+                        )
+                    }
                 }
             }
         }
@@ -214,15 +224,16 @@ private fun DynamicRoundedCornerColumn(
     Column(
         modifier = modifier
             .shadow(
-                elevation = if (isCornersRounded) 4.dp else 0.dp,
+                elevation = if (isCornersRounded) 8.dp else 0.dp,
                 shape = if (isCornersRounded) {
-                    RoundedCornerShape(16.dp)
-                } else RectangleShape
+                    RoundedCornerShape(24.dp)
+                } else RectangleShape,
+                spotColor = Color.Black.copy(alpha = 0.2f)
             )
             .background(
                 color = MaterialTheme.colorScheme.surface,
                 shape = if (isCornersRounded) {
-                    RoundedCornerShape(16.dp)
+                    RoundedCornerShape(24.dp)
                 } else RectangleShape
             )
     ) {
--- feature/chat/presentation/src/commonMain/kotlin/com/rfcoding/chat/presentation/chat_detail/components/MessageBox.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/rfcoding/chat/presentation/chat_detail/components/MessageBox.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.input.TextFieldState
@@ -47,8 +46,7 @@ fun MessageBox(
     val isConnected = connectionState == ConnectionState.CONNECTED
     ChirpMultiLineTextField(
         state = messageTextFieldState,
-        modifier = modifier
-            .padding(4.dp),
+        modifier = modifier,
         placeholder = stringResource(Res.string.send_message),
         keyboardOptions = KeyboardOptions(
             imeAction = ImeAction.Send,
