34fde5a1ce425f110c242b73df43d7d356fe82c5
Riley Farro <rileyfarro@gmail.com>
2025-11-25 19:04:53 +0800

    - Initialize websocket connection immediately when the user land on the chat list.

--- feature/chat/presentation/src/commonMain/kotlin/com/rfcoding/chat/presentation/chat_list_detail/ChatListDetailViewModel.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/rfcoding/chat/presentation/chat_list_detail/ChatListDetailViewModel.kt
@@ -1,14 +1,33 @@
 package com.rfcoding.chat.presentation.chat_list_detail
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.rfcoding.chat.domain.chat.ChatConnectionClient
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 
-class ChatListDetailViewModel: ViewModel() {
+class ChatListDetailViewModel(
+    private val client: ChatConnectionClient
+): ViewModel() {
+
+    private var hasInitializedConnection = false
 
     private val _state = MutableStateFlow(ChatListDetailState())
-    val state = _state.asStateFlow()
+    val state = _state
+        .onStart {
+            if (!hasInitializedConnection) {
+                client.chatMessages.launchIn(viewModelScope)
+                hasInitializedConnection = true
+            }
+        }.stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(5_000L),
+            ChatListDetailState()
+        )
 
     fun onAction(action: ChatListDetailAction) {
         when (action) {
