186a846c070c9a7fd007a3de71fa455e063f17db
Riley Farro <rileyfarro@gmail.com>
2025-11-27 00:16:37 +0800

    Create custom Paginator.

---
+++ core/domain/src/commonMain/kotlin/com/rfcoding/core/domain/util/Paginator.kt
@@ -0,0 +1,61 @@
+package com.rfcoding.core.domain.util
+
+import kotlinx.coroutines.ensureActive
+import kotlin.coroutines.coroutineContext
+
+class Paginator<Key, Item>(
+    private val initialKey: Key,
+    private val onLoadUpdated: (Boolean) -> Unit,
+    private val onRequest: suspend (nextKey: Key) -> Result<List<Item>, DataError>,
+    private val getNextKey: suspend (List<Item>) -> Key,
+    private val onError: suspend (DataError?) -> Unit,
+    private val onSuccess: suspend (items: List<Item>, nextKey: Key) -> Unit
+) {
+
+    private var currentKey = initialKey
+    private var isMakingRequest = false
+    private var lastRequestKey: Key? = null
+
+    suspend fun loadNextItems() {
+        if (isMakingRequest) {
+            return
+        }
+
+        if (currentKey != null && currentKey == lastRequestKey) {
+            return
+        }
+
+        isMakingRequest = true
+        lastRequestKey = currentKey
+        onLoadUpdated(true)
+
+        try {
+            currentKey?.let {
+                val result = onRequest(it)
+                when (result) {
+                    is Result.Failure -> {
+                        onError(result.error)
+                    }
+                    is Result.Success -> {
+                        val items = result.data
+                        val nextKey = getNextKey(items)
+
+                        onSuccess(items, nextKey)
+                        currentKey = nextKey
+                    }
+                }
+            }
+        } catch (_: Exception) {
+            coroutineContext.ensureActive()
+            onError(DataError.Remote.UNKNOWN)
+        } finally {
+            onLoadUpdated(false)
+            isMakingRequest = false
+        }
+    }
+
+    fun reset() {
+        currentKey = initialKey
+        lastRequestKey = null
+    }
+}
